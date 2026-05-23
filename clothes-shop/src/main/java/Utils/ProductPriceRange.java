8f5eacc57ac24dafc070430bae49c6adf5712058
duyenlkce182289 <duyenlkce182289@fpt.edu.vn>
2026-05-24 00:22:30 +0700

    add home and detail product

---
+++ clothes-shop/src/main/java/Utils/ProductPriceRange.java
@@ -0,0 +1,59 @@
+package Utils;
+
+public class ProductPriceRange {
+
+    private float minEffective;
+    private float maxEffective;
+    private float minOld;
+    private float maxOld;
+    private boolean hasRange;
+    private boolean hasSale;
+
+    public float getMinEffective() {
+        return minEffective;
+    }
+
+    public void setMinEffective(float minEffective) {
+        this.minEffective = minEffective;
+    }
+
+    public float getMaxEffective() {
+        return maxEffective;
+    }
+
+    public void setMaxEffective(float maxEffective) {
+        this.maxEffective = maxEffective;
+    }
+
+    public float getMinOld() {
+        return minOld;
+    }
+
+    public void setMinOld(float minOld) {
+        this.minOld = minOld;
+    }
+
+    public float getMaxOld() {
+        return maxOld;
+    }
+
+    public void setMaxOld(float maxOld) {
+        this.maxOld = maxOld;
+    }
+
+    public boolean isHasRange() {
+        return hasRange;
+    }
+
+    public void setHasRange(boolean hasRange) {
+        this.hasRange = hasRange;
+    }
+
+    public boolean isHasSale() {
+        return hasSale;
+    }
+
+    public void setHasSale(boolean hasSale) {
+        this.hasSale = hasSale;
+    }
+}
